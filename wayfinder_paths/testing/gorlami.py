14d69a649b07d30be3639fac18c2ec5ee53ce692
felixjchen <31393977+felixjchen@users.noreply.github.com>
2026-03-13 13:26:59 -0400

    Gorlami - Auth and URL simplification (#149)
    * gorlami simplification
    
    * lint and format

--- wayfinder_paths/testing/gorlami.py
+++ wayfinder_paths/testing/gorlami.py
@@ -7,7 +7,7 @@ from loguru import logger
 
 from wayfinder_paths.core.clients.GorlamiTestnetClient import GorlamiTestnetClient
 from wayfinder_paths.core.config import (
-    get_gorlami_api_key,
+    get_api_key,
     get_rpc_urls,
     set_rpc_urls,
 )
@@ -15,7 +15,7 @@ from wayfinder_paths.core.utils import web3 as web3_utils
 
 
 def gorlami_configured() -> bool:
-    return bool(get_gorlami_api_key())
+    return bool(get_api_key())
 
 
 @pytest.fixture
